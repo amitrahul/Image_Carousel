bd54366f54fab637b08c1e3096254479c3683404
Rahul Raj <raj.rahul686@gmail.com>
2025-02-01 13:34:33 +0530

    Updated documentation for relevant functions

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const slides = document.querySelectorAll(".carousel__slide");
 const navdotWrapper = document.querySelector(".carousel__navdots");
 const navdots = document.querySelectorAll(".carousel__navdots button");
 
-const n_slides = slides.length;
-const n_slidesCloned = 1;
+const total_Slides = slides.length;
+const total_slidesCloned = 1;
 let slideWidth = slides[0].offsetWidth;
 let spaceBtwSlides = Number(
   window
@@ -14,31 +14,46 @@ let spaceBtwSlides = Number(
     .slice(0, -2)
 );
 
-function index_slideCurrent() {
+/*
+ * computes the index of the slide currently shown from how much the slide wrapper is scrolled
+ */
+function index_of_CurrentSlide() {
   return Math.round(
-    slideWrapper.scrollLeft / (slideWidth + spaceBtwSlides) - n_slidesCloned
+    slideWrapper.scrollLeft / (slideWidth + spaceBtwSlides) - total_slidesCloned
   );
 }
 
-function goto(index) {
+/*
+  This is handling Nav dot click functionality.
+ -> clicking each navigation dot will reveal its corresponding slide.
+ */
+function handleNavDotPositionSlide(index) {
   slideWrapper.scrollTo(
-    (slideWidth + spaceBtwSlides) * (index + n_slidesCloned),
+    (slideWidth + spaceBtwSlides) * (index + total_slidesCloned),
     0
   );
 }
-for (let i = 0; i < n_slides; i++) {
-  navdots[i].addEventListener("click", () => goto(i));
-}
 
+for (let i = 0; i < total_Slides; i++) {
+  navdots[i].addEventListener("click", () => handleNavDotPositionSlide(i));
+}
+// for provding smooth behaviour, for infinitely-scrolling.
 slideWrapper.classList.add("smooth-scroll");
 
+/*
+ * helper function that applies the ".is-active" class to a navigation dot with the given index
+ */
 function markNavdot(index) {
   navdots[index].classList.add("is-active");
   navdots[index].setAttribute("aria-disabled", "true");
 }
 
+/*
+ * it runs markNavdot() for the one corresponding to the currently shown slide.
+ */
+
 function updateNavdot() {
   const c = index_slideCurrent();
-  if (c < 0 || c >= n_slides) return;
+  if (c < 0 || c >= total_Slides) return;
   markNavdot(c);
 }
